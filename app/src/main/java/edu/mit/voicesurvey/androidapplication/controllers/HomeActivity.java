5722bf27388e563f30fa4b4657e44b20954768d6
gr21783 <gciccarelli@mit.edu>
2015-06-22 21:24:06 -0400

    Stable version with instructions for closing and relaunching app.

--- app/src/main/java/edu/mit/voicesurvey/androidapplication/controllers/HomeActivity.java
+++ app/src/main/java/edu/mit/voicesurvey/androidapplication/controllers/HomeActivity.java
@@ -134,6 +134,7 @@ public class HomeActivity extends Activity implements AsyncResponse {
                                         // if this button is clicked, close
                                         // current activity
                                         HomeActivity.this.finish();
+
                                     }
                                 })
                                 .setNegativeButton("No",new DialogInterface.OnClickListener() {
@@ -149,43 +150,6 @@ public class HomeActivity extends Activity implements AsyncResponse {
 
                         // show it
                         alertDialog.show();
-
-                        /*********************************
-                         *
-                         *
-                         *
-                         *
-                         * 			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                         context);
-
-                         // set title
-                         alertDialogBuilder.setTitle("Your Title");
-
-                         // set dialog message
-                         alertDialogBuilder
-                         .setMessage("Click yes to exit!")
-                         .setCancelable(false)
-                         .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
-                         public void onClick(DialogInterface dialog,int id) {
-                         // if this button is clicked, close
-                         // current activity
-                         MainActivity.this.finish();
-                         }
-                         })
-                         .setNegativeButton("No",new DialogInterface.OnClickListener() {
-                         public void onClick(DialogInterface dialog,int id) {
-                         // if this button is clicked, just close
-                         // the dialog box and do nothing
-                         dialog.cancel();
-                         }
-                         });
-
-                         // create alert dialog
-                         AlertDialog alertDialog = alertDialogBuilder.create();
-
-                         // show it
-                         alertDialog.show();
-                         */
 
 
                     }
